6f14d91ddf05e1af558eb1bbfc67d0a8c38eef5e
horsi2410 <guilherme241098@gmail.com>
2025-06-03 08:21:35 -0400

    sim

--- src/lib/components/SideBar.svelte
+++ src/lib/components/SideBar.svelte
@@ -7,6 +7,8 @@
       <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
         <li class="nav-item"><a class="nav-link active" href="/todolist">Lista de Tarefas</a></li>
         <li class="nav-item"><a class="nav-link active" href="/senha">Gerador de Senhas</a></li>
+        <li class="nav-item"><a class="nav-link active" href="/calculadora">Calculadora</a></li>
+        <li class="nav-item"><a class="nav-link active" href="/unidades">Conversor de unidades</a></li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown">Dropdown</a>
           <ul class="dropdown-menu">
